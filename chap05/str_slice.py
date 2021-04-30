c698eaa98547aa466d376170f87860dbc59f0814
usako <usako@usako-MacBook-Air.local>
2021-04-30 15:42:34 +0900

    部分文字列を取得

---
+++ chap05/str_slice.py
@@ -0,0 +1,12 @@
+title = 'あいうえおかきくけこ'
+
+print(title[2])
+print(title[2:5])
+print(title[2:])  # うえおかきくけこ
+print(title[:5])  # あいうえお
+print(title[:])  # あいうえおかきくけこ
+print(title[-7:])  # えおかきくけこ
+print(title[-7:-5])  # えお
+print(title[::2])  # あうおきけ
+print(title[1::2])  # いえかくこ
+print(title[::-1])  # こけくきかおえういあ
