8d639fb9cf173fd58a1d278f5dd621e376fef416
usako <usako@usako-MacBook-Air.local>
2021-05-01 13:01:10 +0900

    文字列の値を修正

--- chap05/str_find.py
+++ chap05/str_find.py
@@ -1,8 +1,8 @@
-msg = 'にわにわにわにわとりがいる'
+msg = 'にわにはにわにわとりがいる'
 
 print(msg.find('にわ')) # 0
 print(msg.find('にも')) # -1
 print(msg.rfind('にわ'))  # 6
 print(msg.find('にわ', 3))  # 4
-print(msg.find('にわ', 3, 5))  # -1 なのが謎
+print(msg.find('にわ', 3, 5))  # -1
 print(msg.find('にわ', -7, -1))  # 6
