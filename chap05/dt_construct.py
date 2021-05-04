fc0a2a222e849e9339f937c3644bb36bd8553ccd
usako <usako@usako-MacBook-Air.local>
2021-05-04 15:12:12 +0900

    指定された年月日、時分秒から生成

---
+++ chap05/dt_construct.py
@@ -0,0 +1,8 @@
+import datetime
+
+print(datetime.datetime(2019, 12, 4, 15, 35, 58, 469))
+print(datetime.datetime(2019, 12, 4, 15, 35, 58, 469, datetime.timezone(datetime.timedelta(hours=9))))
+print(datetime.date(2019,12,4))
+print(datetime.time(15, 35, 58, 469))
+print(datetime.time(15, 35, 58, 469, datetime.timezone(datetime.timedelta(hours=9))))
+print(datetime.datetime(2019, 13, 4, 15, 35, 58, 469))
