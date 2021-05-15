40e09cfa80a6883ebcbc037b4e99db1dc81f6861
usako <usako1124.aibo@gmail.com>
2021-05-15 12:19:07 +0900

    出力結果をコメントで追加

--- chap07/dict_trans.py
+++ chap07/dict_trans.py
@@ -1,3 +1,3 @@
 d = {'apple': 'りんご', 'orange': 'みかん', 'melon': 'メロン'}
 result = { value: key for key, value in d.items() }
-print(result)
+print(result)  # { 'りんご': 'apple', 'みかん': 'orange', 'メロン': 'melon' }
