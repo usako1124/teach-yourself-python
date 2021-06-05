dac57d4b670832fa663fd31e046b4a8e33189c1a
usako <usako1124.aibo@gmail.com>
2021-06-05 11:51:56 +0900

    特定のメンバーだけをインポート

--- chap09/figure.py
+++ chap09/figure.py
@@ -6,6 +6,6 @@ def get_triangle(base, height):
 def get_circle(radius):
     return radius * radius * math.pi
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     print(f'三角形の面積：{get_triangle(10, 2)}')
     print(f'円の面積：{get_circle(5)}')
---
+++ chap09/import_from.py
@@ -0,0 +1,2 @@
+from figure import get_triangle
+print(get_triangle(10, 5))
