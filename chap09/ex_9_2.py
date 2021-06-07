4eff9e04e173510a506db1c2cb8c911e56b87a40
usako <usako1124.aibo@gmail.com>
2021-06-07 21:38:25 +0900

    練習問題 9.2

---
+++ chap09/ex_9_2.py
@@ -0,0 +1,17 @@
+# [1]①
+from math import abs
+from math import ceil
+from math import floor
+
+# [1]②
+from figure import *
+__all__ = ['hoge', 'piyo']
+def hoge():
+    print('hoge')
+
+def piyo():
+    print('piyo')
+
+# [1]③
+import sys
+sys.path.append('c:/data')
