307611448e0c1fcd815501504c311614b2e45304
usako <usako1124.aibo@gmail.com>
2021-06-21 23:34:36 +0900

    コンテキストマネージャー

---
+++ chap11/except_context.py
@@ -0,0 +1,21 @@
+class MyContext:
+    # コンテキストの作成
+    def __enter__(self):
+        print('**Enter**')
+        return self
+
+    # コンテキストの開放
+    def __exit__(self, type, value, tb):
+        # 例外の有無を判定
+        if type is None:
+            print('**Exit**')
+        else:
+            print(f'**{value}**')
+    
+    def hoge(self):
+        print('Hoge')
+
+with MyContext() as c:
+    print('With Start')
+    # c.hoge()
+    raise ValueError('値が不正です。')
