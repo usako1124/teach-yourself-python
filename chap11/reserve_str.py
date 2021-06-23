080bc424d8fb044df75a3a46da60e5f5222f6232
usako <usako1124.aibo@gmail.com>
2021-06-23 22:13:13 +0900

    解析可能な表現を取得

--- chap11/reserve_str.py
+++ chap11/reserve_str.py
@@ -4,8 +4,10 @@ class Person:
         self.__lastname = lastname
 
     # インスタンスの文字列表現を生成
-    def __str__(self):
-        return f'{self.lastname} {self.firstname}'
+    # def __str__(self):
+    #     return f'{self.lastname} {self.firstname}'
+    def __repr__(self):
+        return f'Person({self.firstname}, {self.lastname})'
 
     # プロパティを定義
     @property
