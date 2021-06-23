95c8d6bd6c471834ffcdcd896eed621dbf7378f8
usako <usako1124.aibo@gmail.com>
2021-06-23 22:09:24 +0900

    オブジェクトの文字列表現を取得

---
+++ chap11/reserve_str.py
@@ -0,0 +1,21 @@
+class Person:
+    def __init__(self, firstname, lastname):
+        self.__firstname = firstname
+        self.__lastname = lastname
+
+    # インスタンスの文字列表現を生成
+    def __str__(self):
+        return f'{self.lastname} {self.firstname}'
+
+    # プロパティを定義
+    @property
+    def firstname(self):
+        return self.__firstname
+
+    @property
+    def lastname(self):
+        return self.__lastname
+
+if __name__ == '__main__':
+    p = Person('太郎', '山田')
+    print(p)
