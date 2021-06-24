c33cde05551ea3e3c2f8e38880d52ea4ce2aa661
usako <usako1124.aibo@gmail.com>
2021-06-24 16:58:53 +0900

    複合代入演算子の例

---
+++ chap11/reserve_add3.py
@@ -0,0 +1,37 @@
+class Coordinate:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    # Coordinate 同士の加算
+    def __add__(self, other):
+        # 右オペランドの型によって処理を分岐
+        if isinstance(other, Coordinate):
+            # Coordinate 同士の加算
+            return Coordinate(
+                self.x + other.x,
+                self.y + other.y
+            )
+        elif isinstance(other, int):
+            # Coordinate + int の加算
+            return Coordinate(
+                self.x + other,
+                self.y
+            )
+        else:
+            # それ以外の型は不可
+            raise TypeError('type must be Coordinate or int')
+    
+    def __iadd__(self, other):
+        self.x += other.x
+        self.y += other.y
+        return self
+
+    def __str__(self):
+        return f'({self.x}, {self.y})'
+
+if __name__ == '__main__':
+    c1 = Coordinate(10, 20)
+    c2 = Coordinate(15, 25)
+    c1 += c2
+    print(c1)
